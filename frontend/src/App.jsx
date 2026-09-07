d2c542edb02eed57152a52789eb6dc1a3cf65971
Venu <cholletivenumadhur@gmail.com>
2026-09-07 19:16:49 +0530

    First day of building added static ui and cleared and organized the folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App(){
+  return (
+    <div>
+      <h1>NURA</h1>
+      <p>Your Health. Your People. Connected </p>
+      <button type="button">My Health</button>
+      <p>Manage your personal health information</p>
+      <button type="button">My People</button>
+      <p>Manage and connect with your people</p>
+    </div>
+  )
+}
+
+
+export default App
